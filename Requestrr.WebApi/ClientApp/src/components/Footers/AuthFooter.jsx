0a969c32c146c08e8129d8d703e36e801fafc1a9
Darkalfx <darkalfx@protonmail.com>
2020-04-08 21:40:13 -0400

    Updated version

--- Requestrr.WebApi/ClientApp/src/components/Footers/AuthFooter.jsx
+++ Requestrr.WebApi/ClientApp/src/components/Footers/AuthFooter.jsx
@@ -31,7 +31,7 @@ class AuthFooter extends React.Component {
               <Col xl="6">
                 <div className="copyright text-center text-xl-left text-muted">
                   © {new Date().getFullYear()}{" "}
-                  Requestrr (v1.0.9)
+                  Requestrr (v1.0.10)
                 </div>
               </Col>
             </Row>
