0c0ae8aa512f022f226db2977c1abd9d98c4b7d4
Darkalfx <darkalfx@protonmail.com>
2021-03-14 13:38:22 -0400

    Update to .net 5.0

--- Requestrr.WebApi/ClientApp/src/components/Footers/AuthFooter.jsx
+++ Requestrr.WebApi/ClientApp/src/components/Footers/AuthFooter.jsx
@@ -31,7 +31,7 @@ class AuthFooter extends React.Component {
               <Col xl="6">
                 <div className="copyright text-center text-xl-left text-muted">
                   © {new Date().getFullYear()}{" "}
-                  Requestrr (v1.0.16)
+                  Requestrr (v1.0.17)
                 </div>
               </Col>
             </Row>
